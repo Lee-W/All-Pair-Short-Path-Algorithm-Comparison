4b3ece68b8453b31ff503a58784b9b5a5f401c57
LeeW <cl87654321@gmail.com>
2015-06-01 22:09:50 +0800

    Add checkCorrectness to Comparison

--- src/APSPComparison.cpp
+++ src/APSPComparison.cpp
@@ -9,6 +9,70 @@ void printUsage()
     cout << "Usage" << endl;
 }
 
+void checkCorectness(int nodeNum, vector<Arc> arcs)
+{
+    SSSPAlgorithms ssSPFA, ssDial, ssDij, ssPAPE;
+    APSPAlgorithms apFWG, apFWA;
+
+    ssSPFA.initial(nodeNum, arcs);
+    ssDial.initial(nodeNum, arcs);
+    ssDij.initial(nodeNum, arcs);
+    ssPAPE.initial(nodeNum, arcs);
+    apFWG.initial(nodeNum, arcs);
+    apFWA.initial(nodeNum, arcs);
+
+    apFWG.GraphicalFloydWarshall();
+    apFWA.AlgebraicalFloydWarshall();
+
+    vector<vector<int> > fwgDis = apFWG.getDis();
+    vector<vector<int> > fwaDis = apFWA.getDis();
+    vector<int> SPFADis, DialDis, dijDis, PAPEDis;
+    // for (int i = 1; i < nodeNum+1; i++) {
+    //     for (int j = 1; j < nodeNum+1; j++)
+    //         cout << fwgDis[i][j] << "\t";
+    //     cout << endl;
+    // }
+    // for (int i = 1; i < nodeNum+1; i++) {
+    //     for (int j = 1; j < nodeNum+1; j++)
+    //         cout << fwaDis[i][j] << "\t";
+    //     cout << endl;
+    // }
+
+    for (int i = 1; i < nodeNum+1; i++) {
+    // for (int i = 1; i < 2; i++) {
+        ssSPFA.SPFA(i);
+        SPFADis = ssSPFA.getDis();
+
+        ssDial.Dial(i);
+        DialDis = ssDial.getDis();
+
+        ssDij.DijkstraWithBinaryHeap(i);
+        dijDis = ssDij.getDis();
+
+        ssPAPE.PAPE(i);
+        PAPEDis = ssPAPE.getDis();
+
+        for (int j = 1; j < nodeNum + 1; j++) {
+            // if (fwaDis[i][j] != SPFADis[j])
+                // cout << "FWA SPFA Error " << fwaDis[i][j] << "\t" << SPFADis[j] << endl;
+            // if (fwaDis[i][j] != PAPEDis[j])
+                // cout << "FWA PAPE Error" << endl;
+            if (fwaDis[i][j] != dijDis[j]) {
+                cout << i << "\t" << j;
+                cout << "FWA Dij Error " << fwaDis[i][j] << "\t" << dijDis[j] <<endl;
+            }
+            if (fwaDis[i][j] != DialDis[j]) {
+                cout << i << "\t" << j;
+                cout << "FWA Dial Error " << fwaDis[i][j] << "\t" << DialDis[j] <<endl;
+            }
+            // if (fwaDis[i][j] != fwgDis[i][j])
+                // cout << "FWA FWG Error" << endl;
+        }
+        cout << endl;
+    }
+    exit(0);
+}
+
 int main(int argc, const char *argv[])
 {
     int nodeNum;
@@ -19,6 +83,8 @@ int main(int argc, const char *argv[])
         fr.readFile(argv[1]);
         nodeNum = fr.getNodeNum();
         arcs = fr.getArcs();
+
+        checkCorectness(nodeNum, arcs);
 
         SSSPAlgorithms ss;
         APSPAlgorithms ap;
