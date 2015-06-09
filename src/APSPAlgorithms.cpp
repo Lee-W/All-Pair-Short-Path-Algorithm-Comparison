9a3e9f036c42df9c80e0ca15852ca433bd4dd77f
LeeW <cl87654321@gmail.com>
2015-06-09 17:24:43 +0800

    Finish FWG implementation

--- src/APSPAlgorithms.cpp
+++ src/APSPAlgorithms.cpp
@@ -72,32 +72,32 @@ void APSPAlgorithms::GraphicalFloydWarshall()
     relaxNum = 0;
 
     // GraphicalFloydWarshall initialization
-    vector<vector<Arc> > fromList, toList;
+    vector<map<int, Arc> > fromList, toList;
     fromList.resize(nodeNum + 1);
     toList.resize(nodeNum + 1);
     for (auto arc: arcs) {
-        fromList[arc.from].push_back(arc);
-        toList[arc.to].push_back(arc);
+        fromList[arc.from][arc.to] = arc;
+        toList[arc.to][arc.from] = arc;
     }
 
+    Arc startArc, endArc;
     for (int mid = 1; mid < nodeNum + 1; mid++)
-    {
-        for (auto startArc: toList[mid])
-            for (auto endArc: fromList[mid]) {
+        for (auto startArcEntry: toList[mid])
+            for (auto endArcEntry: fromList[mid]) {
                 relaxNum++;
+
+                startArc = startArcEntry.second;
+                endArc = endArcEntry.second;
 
                 int temp = startArc.arcLength + endArc.arcLength;
                 if (dis[startArc.from][endArc.to] > temp) {
-                    if (dis[startArc.from][endArc.to] == INF) {
-                        fromList[startArc.from].push_back(Arc(startArc.from, endArc.to, temp));
-                        toList[endArc.to].push_back(Arc(startArc.from, endArc.to, temp));
-                    }
+                    fromList[startArc.from][endArc.to] = Arc(startArc.from, endArc.to, temp);
+                    toList[endArc.to][startArc.from] = Arc(startArc.from, endArc.to, temp);
 
                     dis[startArc.from][endArc.to] = temp;
                     pre[startArc.from][endArc.to] = mid;
                 }
             }
-    }
 
     processTime = clock() -startTime;
 }
--- src/APSPAlgorithms.h
+++ src/APSPAlgorithms.h
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <ctime>
 #include <vector>
+#include <map>
 #include <algorithm>
 #include "Arc.h"
 using namespace std;
